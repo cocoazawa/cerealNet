ed2a22e61c628fdac735a920b92038a5be74bf5f
peter89SMIS <peter89@smis.ac.jp>
2024-04-25 10:18:41 +0900

    active and non-active indicator

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 "use client"
 
 import Card from "./elements.jsx";  // This is the "Card" tag that I created to simplify the addition of glass texture information cards.
-import { useEffect } from "react";  // useEffect runs whenever the webpage loads, and when this runs can differ based on a multitude of factors but for this project, it runs on load.
+import { useEffect, useState } from "react";  // useEffect runs whenever the webpage loads, and when this runs can differ based on a multitude of factors but for this project, it runs on load.
 
-let connection = new WebSocket("ws://localhost:8765"); // WebSocket connection to my Python Server
+let connection: WebSocket; // WebSocket connection to my Python Server
 
 export default function Home() {
+  let [colorOfActive, setColorOfActive] = useState("red");
+  
   useEffect(() => {
+    connection = new WebSocket("ws://localhost:8765"); // WebSocket connection to my Python Server
+    connection.onopen =  (event) => {
+      setColorOfActive("green")
+      document.getElementById("dataComms1Text")!.textContent = `Active`  // Altering the DOM textContent to write the 1 or 0.
+    };
+    connection.addEventListener("close", (event) => {
+      setColorOfActive("red")
+      document.getElementById("dataComms1Text")!.textContent = `Not Active`  // Altering the DOM textContent to write the 1 or 0.
+    });
     connection.addEventListener("message", (event) => {
+      console.log(event.data);
       document.getElementById("dataPoint1AText")!.textContent = `${event.data}`  // Altering the DOM textContent to write the 1 or 0.
     });
   }, []);
@@ -54,12 +66,25 @@ export default function Home() {
           <div>
             <div className="rowIconText">
               <span className="material-symbols-outlined">monitoring</span>
-              <h1>Data Communications 1A</h1>
+              <h1>Data Communications 1B</h1>
             </div>
             <p>Custom data communications.</p>
             <div className="buttonDiv">
               <button onClick={sendLedOn}>Turn Lights ON</button> {/* these are the two On/Off Buttons. */}
               <button onClick={sendLedOff}>Turn Lights OFF</button>
+            </div>
+          </div>
+        } type={"smallRight"}></Card>
+        <Card content={
+          <div>
+            <div className="rowIconText">
+              <span className="material-symbols-outlined">sensors</span>
+              <h1>Comm Link 1</h1>
+            </div>
+            <p>Custom data communications.</p>
+            <div className="rowIconText">
+              <span className={`dot ${colorOfActive}`}></span>
+              <h1 id="dataComms1Text">Not Active</h1>
             </div>
           </div>
         } type={"smallRight"}></Card>
